7e3d3e1799ca2f21821fc2394a9165419c98d703
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-05 13:37:04 +0530

    [Rakshit] Add: coalscaling operator

---
+++ Practice-5_12_24/coalScaling.js
@@ -0,0 +1,11 @@
+let username = 0;
+let defaultUser = "Guest";
+
+//coalScaling operator ?? will return the right operand if the left operand is null or undefined
+// let name = username ?? defaultUser
+// console.log(name)
+
+//logical OR operator will return the right operand if the left operand is falsy
+let rightValue = 1
+let leftValue = 0
+console.log(leftValue || rightValue)
