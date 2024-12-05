b96d9fc83c0b43f8d9f29e57218340d5062a4d8a
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-05 13:54:18 +0530

    [Rakshit] Add

---
+++ Practice-5_12_24/encapsulation.js
@@ -0,0 +1,21 @@
+class Encapsulation{
+    //# is used to set private variables in class
+    #name;
+
+    constructor(){
+        this.#name = "Rakshit";
+    }
+
+    getName(){
+        return this.#name;
+    }
+
+    setName(name){
+        this.#name = name;
+    }
+}
+
+let obj = new Encapsulation();
+console.log(obj.getName());
+obj.setName("Raghav");
+console.log(obj.getName());
