fed2e2892692d1cb34b9d9365ea4ec963b4c8ff3
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-06 14:10:08 +0530

    [Rakshit] Add: scheduling - setTimeout() and setInterval()

---
+++ Practice-6_12_24/scheduling.js
@@ -0,0 +1,13 @@
+//setTimeout
+// const timer = setTimeout(()=>{
+//     console.log("Hello") //wuill be printed after 5 seconds
+// },5000)
+
+// clearTimeout(timer) //clears the timer
+
+//setInterval
+const interval = setInterval(()=>{
+    console.log("Hello") //will be printed every 5 seconds
+},5000)
+
+clearInterval(interval) //clears the interval
