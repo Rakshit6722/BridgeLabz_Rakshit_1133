79c010e9626ce32b91848f7ef0d603ab8a98e021
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-11 11:38:20 +0530

    [Rakshit] Refactor: fibonacci series from 1 to 10, craete a closure within function

--- ques14.js
+++ ques14.js
@@ -1,10 +1,17 @@
-let a = 0;
-let b = 1;
-
-for (let i = 1; i <= 10; i++) {
-    console.log(a);
-    let c = a + b;
-    a = b;
-    b = c;
+function findFibSeries(){
+    let a = 0;
+    let b = 1;
+    return function(){
+        for (let i = 1; i <= 10; i++) {
+            console.log(a);
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+    }
 }
+
+let fibSeries = findFibSeries();
+fibSeries();
+
 
