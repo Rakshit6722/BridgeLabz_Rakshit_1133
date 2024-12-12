20025ba6168b0ed9a39cca25cbd3b2b08b47fb5f
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-12 14:03:32 +0530

    [Rakshit] Add: fibonacci with highest precision

---
+++ ques53.js
@@ -0,0 +1,24 @@
+//with for loop
+// const fib = (n) => {
+//     let arr = [0, 1]
+//     for(let i = 2; i < n; i++){
+//         arr[i] = arr[i-1] + arr[i-2]
+//     }
+//     return arr[arr.length-1]
+// }
+
+// with dp
+const fib = (n) => {
+    let dp = new Array(n+1)
+    dp.fill(-1)
+    return helperFunc(n,dp)
+}
+
+function helperFunc(n,dp){
+    if(n===0 || n===1) return n
+    if(dp[n] !== -1) return dp[n]
+    dp[n] = helperFunc(n-1,dp) + helperFunc(n-2,dp)
+    return dp[n]
+}
+
+console.log(fib(500))
