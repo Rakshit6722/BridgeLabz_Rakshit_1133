685c81309833498a0b85aa725065db5a58577a4b
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-12 09:52:19 +0530

    [Rakshit] Refactor: capitalize first letter

--- ques32.js
+++ ques32.js
@@ -1,9 +1,10 @@
 const capitalizeFirstLetter = (str) => {
-    let arr = str.trim().split(/\s+/);
-    const res = arr.map(element => (
-    element.slice(0,1).toUpperCase() + element.slice(1)
-    ));
-    return res.join(" ");
+    // let arr = str.trim().split(/\s+/);
+    // const res = arr.map(element => (
+    // element.slice(0,1).toUpperCase() + element.slice(1)
+    // ));
+    // return res.join(' ');
+    return str.replace(/\b\w/g, char => char.toUpperCase());
 }
 
-console.log(capitalizeFirstLetter("hello  world"));
+console.log(capitalizeFirstLetter("hello         world"));
