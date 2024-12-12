8a76b17726b899d9938db14438a3a6b73300db5b
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-12 15:27:09 +0530

    [Rakshit] Add: handle case of a single number in jagged array

--- ques45.js
+++ ques45.js
@@ -1,11 +1,15 @@
 const findSum = (arr) => {
     let sum = 0;
     arr.forEach(element => {
-        element.forEach(num => {
-            sum += num;
-        });
+        if(Array.isArray(element)){
+            element.forEach(num => {
+                sum += num;
+            });
+        }else{
+            sum += element;
+        }
     });
     return sum
 }
 
-console.log(findSum([[1,2,3],[4,6],[7]])) 
+console.log(findSum([[1,2,3],[4,6],32,[7]])) 
