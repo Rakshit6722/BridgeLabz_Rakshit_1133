5a1164337caf0597a626ae9e532f89468436ec96
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-13 13:42:30 +0530

    [Rakshit] Add: stack implementation

---
+++ Stack/implementation.js
@@ -0,0 +1,38 @@
+class Stack{
+    #stack
+    constructor(){
+        this.#stack = []
+    }
+
+    push(...args){
+        this.#stack.push(...args)
+    }
+
+    pop(){
+        return this.#stack.pop()
+    }
+
+    peek(){
+        return this.#stack[this.#stack.length - 1]
+    }
+
+    isEmpty(){
+        return this.#stack.length === 0
+    }
+
+    print(){
+        console.log(this.#stack)
+    }
+}
+
+let stack = new Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.print()
+console.log(stack.pop())
+stack.print()
+console.log(stack.peek())
+console.log(stack.isEmpty())
+stack.print()
+console.log(stack.pop())
