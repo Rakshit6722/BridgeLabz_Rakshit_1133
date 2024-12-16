d6f3e269c915a2eb4dc466ea9b213d7c04325a97
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-12-16 17:16:20 +0530

    [Rakshit] Refactor: Add deep and shallow copy function

--- reviewArray.js
+++ reviewArray.js
@@ -42,3 +42,66 @@ class CustomArray extends Array{
 
 let customArray = new CustomArray()
 customArray.operations(4,21,3,2,44,'a','b','a',true,false)
+
+
+const copyArrObject = (arr) => {
+    let newArr = []
+    for(let i=0;i<arr.length;i++){
+        if(i%2 === 0){
+            newArr.push(shallowCopy(arr[i]))
+        }else{
+            newArr.push(deepCopy(arr[i]))
+        }
+    }
+
+    return newArr
+}
+
+const shallowCopy = (obj) => {
+    // let newObj = Object.assign({},obj)
+    let newObj = obj
+    return newObj
+}
+
+const deepCopy = (obj) => {
+    // let newObj = JSON.parse(JSON.stringify(obj))
+    // return newObj
+    if(typeof obj !== 'object' || obj === null){
+        return obj
+    }
+
+    let newObj = Array.isArray(obj) ? [] : {}
+    let keys = Object.keys(obj)
+
+    for(let key of keys){
+        newObj[key] = deepCopy(obj[key])
+    }
+
+    return newObj
+}
+
+let arr = [{a:1,b:2,e:{f:3}},{c:3,d:4},{e:5,f:6}]
+let arr2 = copyArrObject(arr)
+
+arr2[1].c = 10
+
+// console.log(arr[1].c)
+// console.log(arr2[1].c)
+
+// arr2[0].e.f = 10
+
+// console.log(arr[0].e.f)
+// console.log(arr2[0].e.f)
+
+arr2[1].c = 10
+console.log(arr[1].c)
+console.log(arr2[1].c)
+
+// arr2[0].a = 10
+
+
+// console.log(arr[0].a)
+// console.log(arr2[0].a)
+
+
+
