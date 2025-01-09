a0c6203a902afdc204823e9cd03515bc7bdab9f9
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-01-09 15:35:33 +0530

    [Rakshit] Refactor: fetching api through promise

--- promises.js
+++ promises.js
@@ -1,17 +1,55 @@
 // Basic implemeneation of promises
 
-let age = 15;
+// let age = 15;
 
-let promise = new Promise((res, rej) => {
-    if (age >= 18) {
-        res("Resolved")
-    } else {
-        rej("Rejected")
-    }
-})
+// let promise = new Promise((res, rej) => {
+//     if (age >= 18) {
+//         res("Resolved")
+//     } else {
+//         rej("Rejected")
+//     }
+// })
 
-promise.then((data) => {
-    console.log(data)
-}).catch((err) => {
-    console.log(err)
-})
+// promise.then((data) => {
+//     console.log(data)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+
+//fetching data from api with promsies
+
+//one way to do it
+const fetchData = (url) => {
+    return new Promise((res,rej)=>{
+        const response = fetch(url)
+        if(response){
+            res(response)
+        }else{
+            rej("Error fetching data")
+        }
+    })
+}
+
+fetchData("https://jsonplaceholder.typicode.com/todos/2")
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log("first")
+        console.log(data)
+    })
+    .catch(err => console.log(err))
+
+
+//other way to do it directly using fetch
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log("second")
+        console.log(data)
+    })
+    .catch(err => console.log(err))
