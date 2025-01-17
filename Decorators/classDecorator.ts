502be2e48370266ce0e23999904f3a508faec957
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-01-17 11:25:17 +0530

    [Rakshit] Refactor: decorator factory

--- Decorators/classDecorator.ts
+++ Decorators/classDecorator.ts
@@ -13,19 +13,54 @@
 // const person3 = new Person2("John")
 
 //multiple decorator
-function first(constructor: Function){
-    console.log(`First decorator`)
+// function first(constructor: Function){
+//     console.log(`First decorator`)
+// }
+
+// function second(constructor: Function){
+//     console.log(`Second decorator`)
+// }
+
+// @first 
+// @second
+// class Example{
+
+// }
+
+// const example1 = new Example()
+
+// function first() {
+//     console.log("first(): factory evaluated");
+//     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+//       console.log("first(): called");
+//     };
+//   }
+  
+//   function second() {
+//     console.log("second(): factory evaluated");
+//     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+//       console.log("second(): called");
+//     };
+//   }
+  
+//   class ExampleClass {
+//     @first()
+//     @second()
+//     method() {}
+//   }
+  
+
+function sealed(constructor: Function){
+    Object.seal(constructor)
+    Object.seal(constructor.prototype)
 }
 
-function second(constructor: Function){
-    console.log(`Second decorator`)
+@sealed
+class BugReport{
+    type = "report"
+    title: string
+
+    constructor(t: string){
+        this.title = t
+    }
 }
-
-@first 
-@second
-class Example{
-
-}
-
-const example1 = new Example()
-
